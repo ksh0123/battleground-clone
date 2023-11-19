2e111970927c734f433450b4415ff348d60707f7
ksh0123 <shnae.k@gmail.com>
2023-11-20 01:10:13 +0900

    Add footer copy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import FooterCopy from "./components/FooterCopy";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <section className="bg-black">
+      <div className="bg-red-100">Menu</div>
+      <section className="bg-orange-500 w-[837px] h-[955px] mx-auto">
+        interactive banner
+      </section>
+      <section className="bg-yellow-300 mx-auto mb-20 w-[720px] h-[1148px]">
+        image scrolls into video
+      </section>
+      <section className="bg-green-200 mx-auto w-[991px] h-[350px] mb-[153.6px]">
+        game promo text 1
+      </section>
+      <section className="bg-green-200 mx-auto w-[991px] h-[350px] mb-[153.6px]">
+        game promo text 2
+      </section>
+      <section className="bg-blue-200 mx-auto w-[720px] h-[202px] mb-[153.6px]">
+        counter
+      </section>
+      <section className="bg-purple-200 mx-auto w-[720px] h-[530px] mb-20">
+        Awards
+      </section>
+      <section className="bg-purple-200 mx-auto w-[720px] h-[347px] mb-20">
+        Game Platform
+      </section>
+      <section className="bg-purple-400 mx-auto w-[720px] h-[171px] mb-20">
+        Bottom Banner
+      </section>
+      <section className="bg-purple-950 w-[1103px] mx-auto mt-[166px] h-[70px] border-b-[1px] border-slate-500">
+        Footer
+      </section>
+      <FooterCopy />
+    </section>
+  );
 };
 
 export default App;
---
+++ src/components/FooterCopy.jsx
@@ -0,0 +1,24 @@
+import { GoPlus } from "react-icons/go";
+
+const FooterCopy = () => {
+  return (
+    <section className="w-[1103px] mx-auto mt-[65px] flex justify-between items-center pt-[32px] pb-[58px]">
+      <div className="flex flex-col">
+        <img
+          src="https://www.krafton.com/wp-content/themes/krafton/assets/img/logo/logo-white.png"
+          className="mb-[11px] w-[111px]"
+          alt="footer logo"
+        />
+        <div className="text-[#FFFFFF66] text-xs">
+          © 2021 KRAFTON, Inc. All rights reserved.
+        </div>
+      </div>
+      <div className="text-white flex justify-between items-center w-[227px] border-b-[1px]">
+        <div className="text-xs mb-2">FAMILY SITE</div>
+        <GoPlus className="mb-1" />
+      </div>
+    </section>
+  );
+};
+
+export default FooterCopy;
